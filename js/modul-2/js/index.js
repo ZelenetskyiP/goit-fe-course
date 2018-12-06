0f96fba8f6c9571b40b1d65fea3077848720e4a4
Zeleneteskyipasha <Zelenetskyipaha@gmail.com>
2018-12-06 21:37:57 +0200

    new

--- js/modul-2/js/index.js
+++ js/modul-2/js/index.js
@@ -5,25 +5,22 @@ const numbers = [];
 let total = 0;
 
 do {
-  userInput = prompt("Введите число", "");
-  if (isNaN(userInput)) {
-    alert("Было введено не число, попробуйте еще раз");
-  } else if (userInput) {
-    numbers.push(Number(userInput));
-  }
-  
-} while (userInput !== null);
+  userInput = Number(prompt("Введите число", ""));
 
-for (let sum of numbers) {
-  total += sum;
-}
-if (total) {
+  if (Number.isNaN(userInput)) {
+    alert("Было введено не число, попробуйте еще раз");
+  } else {
+    numbers.push(userInput);
+  }
+} while (userInput !== 0);
+
+if (numbers.length > 1) {
+  for (let sum of numbers) {
+    total += sum;
+  }
+
   alert(`Общая сумма чисел равна ${total}`);
 }
 
-console.log(total);
-console.log(numbers);
-
-
-
-
+// console.log(total);
+// console.log(numbers);
