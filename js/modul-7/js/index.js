6502efade7eda67bf61b0b6e77327be065ba328e
Zeleneteskyipasha <Zelenetskyipaha@gmail.com>
2019-03-01 00:27:05 +0200

    new

--- js/modul-7/js/index.js
+++ js/modul-7/js/index.js
@@ -1,43 +1,71 @@
-'use strict'
+"use strict";
 
+const posts = [
+  {
+    img: "https://placeimg.com/400/150/arch",
+    title: "Post title 1",
+    text:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga, nemo dignissimos ea temporibus voluptatem maiores maxime consequatur impedit nobis sunt similique voluptas accusamus consequuntur, qui modi nesciunt veritatis distinctio rem!",
+    link: "link-1.com"
+  },
+  {
+    img: "https://placeimg.com/400/150/nature",
+    title: "Post title 2",
+    text:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga, nemo dignissimos ea temporibus voluptatem maiores maxime consequatur impedit nobis sunt similique voluptas accusamus consequuntur, qui modi nesciunt veritatis distinctio rem!",
+    link: "link-2.com"
+  },
+  {
+    img: "https://placeimg.com/400/150/arch",
+    title: "Post title 3",
+    text:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga, nemo dignissimos ea temporibus voluptatem maiores maxime consequatur impedit nobis sunt similique voluptas accusamus consequuntur, qui modi nesciunt veritatis distinctio rem!",
+    link: "link-3.com"
+  }
+];
 
+function createPostCard({ imgF = "", titleF = "", textF = "", linkF = "" }) {
+  const main = document.querySelector(".main");
+  const divv = document.createElement("div");
+  divv.classList.add("post");
 
+  const img = document.createElement("img");
+  img.classList.add("post__image");
+  img.setAttribute("src", imgF);
+  img.setAttribute("alt", "post image");
 
-const movie = document.createElement('div')
-movie.classList.add('movie')
+  const h2 = document.createElement("h2");
+  h2.classList.add("post__title");
+  h2.textContent = titleF;
 
+  const p = document.createElement("p");
+  p.classList.add("post__text");
+  p.textContent = textF;
 
-const img = document.createElement('img');
-img.classList.add('movie__image');
-img.setAttribute('alt', 'movie image');
-img.setAttribute('src', 'http://image.tmdb.org/t/p/w500/rPdtLWNsZmAtoZl9PK7S2wE3qiS.jpg');
+  const btn = document.createElement("a");
+  btn.classList.add("button");
+  btn.setAttribute("href", linkF);
+  btn.textContent = "Read more";
 
-const movieBody  = document.createElement('div');
-movieBody.classList.add('movie__body');
+  main.append(divv);
+  divv.append(img, h2, p, btn);
 
-const h2 = document.createElement('h2');
-h2.classList.add('movie__title');
-h2.textContent = 'The Godfather';
+  return divv;
+}
 
-const pText = document.createElement('p');
-pText.classList.add('movie__description');
-pText.textContent = "Spanning the years 1945 to 1955, a chronicle of the fictional Italian-American Corleone crime family. When organized crime family patriarch, Vito Corleone barely survives an attempt on his life, his youngest son, Michael steps in to take care of the would-be killers, launching a campaign of bloody revenge.";
+function createCards(arr) {
+  const list = [];
+  arr.forEach(item => {
+    const element = createPostCard({
+      imgF: item.img,
+      titleF: item.title,
+      textF: item.text,
+      linkF: item.link
+    });
+    list.push(element);
+  });
+  const rroot = document.querySelector(".main");
+  rroot.append(...list);
+}
 
- 
-const pData = document.createElement('p');
-pData.classList.add('movie__date');
-pData.textContent = 'Released: 1972-03-14';
-
-const pRating = document.createElement('p');
-pRating.classList.add('movie__rating');
-pRating.textContent = 'Rating: 8.6';
-
-
-movie.prepend(img, movieBody);
-movieBody.prepend(h2, pText, pData, pRating);
-
-
-
-console.log(movie)
-
-
+createCards(posts);
